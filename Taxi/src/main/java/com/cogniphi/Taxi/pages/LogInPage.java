dbeaf432ea8a545f7e737eca95860d7710c8f688
github <suresh500@gmail.com>
2018-09-11 15:32:55 +0530

    Initial Commit

---
+++ Taxi/src/main/java/com/cogniphi/Taxi/pages/LogInPage.java
@@ -0,0 +1,27 @@
+package com.cogniphi.Taxi.pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+
+import com.utility.BaseClass;
+
+import atu.testrecorder.exceptions.ATUTestRecorderException;
+
+public class LogInPage extends BaseClass{
+	
+public static final Logger log = Logger.getLogger(LogInPage.class.getName());
+	
+	public static void chrome_LogIn() throws ATUTestRecorderException {
+
+		//BaseClass.beforeTest();
+		log.info("chrome browser Launched");
+		driver.findElement(By.id(reader.get("username_id"))).sendKeys(reader.get("UserName"));
+		log.info("Username Entered");
+		driver.findElement(By.id(reader.get("userpwd_id"))).sendKeys(reader.get("Password"));
+		log.info("Password entered");
+		driver.findElement(By.name(reader.get("LogIn_name"))).click();
+		log.info("Clicked on Login button");
+		driver.manage().window().maximize();
+	}
+
+}
